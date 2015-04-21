210257c001a62c9157f9d067eb8b6d2348ea7256
celian garcia <celian.garcia@utbm.fr>
2015-04-21 11:21:27 +0200

    First android commit

---
+++ android/app/src/main/java/fr/gi/utbm/lo53project/menuDetection_Fragment.java
@@ -0,0 +1,21 @@
+package fr.gi.utbm.lo53project;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Android on 06/04/2015.
+ */
+public class menuDetection_Fragment extends Fragment {
+    View rootView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.menudetection_layout, container, false);
+        return rootView;
+    }
+}
