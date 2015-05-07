f64ad9ed44d4e59421312640eb87283944a64d3a
jnovak <johann.novak@utbm.fr>
2015-05-07 17:50:16 +0200

    hibernate insertion for calibration OK with dao's insertSample

---
+++ javaEE/database/insert_measurements.sql
@@ -0,0 +1,8 @@
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (4, 127.3987, '00-14-22-01-23-45', 5); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (5, 457.36, '00:11:43:00:00:01', 4); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (6, 17.21, '00:11:43:00:00:01', 3); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (7, 161773, '01:80:C2:00:00:00', 2); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (8, 6337.3, '01:77:C2:00:34:00', 1); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (1, 9.3, '01:80:C2:00:00:00', 2); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (2, 9167.63, '01:00:0C:CC:CC:CC', 1); 
+INSERT INTO measurement(measurement_id, rssi, mac_address, position_id) VALUES (3, 9867.3, '00:0a:95:9d:68:16', 6); 
