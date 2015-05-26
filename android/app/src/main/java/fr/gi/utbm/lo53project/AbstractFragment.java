15b47352db09d86f67d185d08f6971024c735c5f
jnovak <johann.novak@utbm.fr>
2015-05-26 13:06:32 +0200

    update runnables in order to communicate with APs => working

--- android/app/src/main/java/fr/gi/utbm/lo53project/AbstractFragment.java
+++ android/app/src/main/java/fr/gi/utbm/lo53project/AbstractFragment.java
@@ -1,33 +1,35 @@
 package fr.gi.utbm.lo53project;
 
+import java.net.URL;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.net.URL;
-
 /**
  * Created by celian on 05/05/15 for LO53Project
  */
-public abstract class AbstractFragment extends Fragment {
+public abstract class AbstractFragment extends Fragment
+{
 
-    protected WorldMap mMap;
-    protected URL mUrl;
+	protected WorldMap	mMap;
+	protected URL		mUrl;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	@Nullable
+	@Override
+	public View onCreateView(
+			final LayoutInflater inflater,
+			final ViewGroup container,
+			final Bundle savedInstanceState)
+	{
 
-        // Load the world map from Main Activity
-        Bundle args = getArguments();
-        mMap = (WorldMap) args.getSerializable(MainActivity.TAG_WORLDMAP);
+		// Load the world map from Main Activity
+		Bundle args = getArguments();
+		mMap = (WorldMap) args.getSerializable(MainActivity.TAG_WORLDMAP);
 
-        return null;
-    }
+		return null;
+	}
 
 }
-
