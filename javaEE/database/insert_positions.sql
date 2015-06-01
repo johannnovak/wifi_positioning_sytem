c9412b2025ab22950eb221943539d33b6af70b0c
johannnovak <johann.novak@utbm.fr>
2015-06-01 23:54:06 +0200

    sql scripts renamed

--- javaEE/database/insert_positions.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO position(position_id, x, y) VALUES (1, 34.9055, 4.77); 
-INSERT INTO position(position_id, x, y) VALUES (2, 50.755, 33244.77); 
-INSERT INTO position(position_id, x, y) VALUES (3, 990.555, 23244.77); 
-INSERT INTO position(position_id, x, y) VALUES (4, 324.575, 44221.77); 
-INSERT INTO position(position_id, x, y) VALUES (5, 3490.535, 4444.89877); 
-INSERT INTO position(position_id, x, y) VALUES (6, 3445.6755, 4784.77 ); 
-
-
